5cfcd70888c437d19a15ba84eb454f3cb3444c21
Nelson Penn <nelsonapenn@gmail.com>
2020-09-06 15:49:36 -0400

    updated driver

--- driver.sh
+++ driver.sh
@@ -4,16 +4,15 @@ GREEN='\033[1;32m'
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 echo -e "${ORANGE}Getting bills...${NC}"
-cd get_bills
-if node index.js; then
+cd bill_tracker_node_suite
+if node get_bills.js; then
   echo -e "${GREEN}Got 'em${NC}"
 else
   zenity --warning --text="Encountered an error scraping some bills."
   echo -e "${RED}Encountered an error scraping some bills.${NC}"
 fi
-echo -e "${ORANGE}Handling bills...${NC}"
-cd ../handle_bills
-if cargo run; then
+echo -e "${ORANGE}Handling bills... dry run${NC}"
+if node handle_bills.js dry; then
   echo -e "${GREEN}Handled 'em${NC}"
 else
   zenity --warning --text="Encountered an error handling some bills."
